26daa7daadf95782b24d5a0040314a688e4f723d
l0r5 <lars-braitinger@web.de>
2021-03-24 22:28:44 +0100

    Use props.children

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
                 <Person name={"Lars"} age={"30"}/>
-                <Person name={"Lars"} age={"30"}>My Hobbies: Racing</Person>
-                <Person name={"Lars"} age={"30"}/>
+                <Person name={"Ralf"} age={"49"}>My Hobbies: Racing</Person>
+                <Person name={"Tina"} age={"22"}/>
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
