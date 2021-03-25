9f23523b89fb1938ff817ca82b251a2c15282f95
l0r5 <lars-braitinger@web.de>
2021-03-25 14:29:42 +0100

    Passing click function to dummy component using different styles

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
         otherState: 'some other value'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({
             persons: [
-                {name: 'Bars', age: '30'},
+                {name: newName, age: '30'},
                 {name: 'Maximilian', age: '28'},
                 {name: 'Stephanie', age: '27'},
             ]
@@ -27,11 +27,18 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My
-                    Hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={() => this.switchNameHandler('Bars')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Mars!')}> // that's the better one
+                    My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
