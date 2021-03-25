5dd5aab00ecfef45aaa962a895c842a7ab5ad9a5
l0r5 <lars-braitinger@web.de>
2021-03-25 17:28:59 +0100

    Extract the persons div into a separate variable that is being injected to the return part of the render function if the boolean is showPersons is true

--- src/App.js
+++ src/App.js
@@ -47,6 +47,27 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Mars!')}
+                        changed={this.nameChangedHandler}>
+                        My Hobbies: Racing</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
@@ -54,25 +75,9 @@ class App extends Component {
                 <button
                     style={style}
                     onClick={this.togglePersonsHandler}>
-                    Switch Name
+                    Toggle Persons
                 </button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age}/>
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'Mars!')}
-                                changed={this.nameChangedHandler}>
-                                My Hobbies: Racing</Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age}/>
-                        </div> : null
-                }
+                {persons}
             </div>
         );
     }
