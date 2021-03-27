075d95b03f225cf896376eac8163c6ec1cd6f00d
l0r5 <lars-braitinger@web.de>
2021-03-27 23:24:55 +0100

    Add StyleRoot to make media-queries work using Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -75,9 +75,9 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
-            style[':hover'] =  {
+            style[':hover'] = {
                 backgroundColor: 'salmon',
-                    color: 'black'
+                color: 'black'
             }
         }
 
@@ -89,16 +89,18 @@ class App extends Component {
             classes.push('bold');
         }
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I'm a React App</h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button
+                        style={style}
+                        onClick={this.togglePersonsHandler}>
+                        Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
     }
 }
