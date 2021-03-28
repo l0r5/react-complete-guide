5bfc5a32856441af0184ef13c34effbd16802092
l0r5 <lars-braitinger@web.de>
2021-03-28 19:33:15 +0200

    Add hoc with classes prop

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass'
 
 class App extends Component {
 
@@ -86,7 +87,7 @@ class App extends Component {
         }
 
         return (
-            <div className={classes.App}>
+            <WithClass classes={classes.App}>
                 <button onClick={() => {
                     this.setState({showCockpit: false})
                 }}>Remove Cockpit
@@ -100,7 +101,7 @@ class App extends Component {
                     /> :
                     null}
                 {persons}
-            </div>
+            </WithClass>
         );
     }
 }
