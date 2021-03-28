862cf15afaf4c6e50eb413c3ef8e981074f0155e
l0r5 <lars-braitinger@web.de>
2021-03-28 20:17:52 +0200

    Use ref to set focus to an input

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,17 @@ import Aux from '../../../hoc/Aux'
 import classes from './Person.css';
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -14,8 +25,13 @@ class Person extends Component {
                 <p key="i1" onClick={this.props.click}> I 'm {this.props.name} and I
                     am {this.props.age} years old!</p>
                 <p key="i2"> {this.props.children}</p>
-                <input key="i3" type={"text"} onChange={this.props.changed}
-                       value={this.props.name}/>
+                <input
+                    key="i3"
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref = {this.inputElementRef}
+                    type={"text"}
+                    onChange={this.props.changed}
+                    value={this.props.name}/>
             </Aux>
         )
     }
