9bb9d6b8f21a04bc56f3ca0797dc3d8dec6345f4
l0r5 <lars-braitinger@web.de>
2021-03-28 20:48:29 +0200

    Use context api to handle auth-context

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import withClass from '../../../hoc/withClass'
 import Aux from '../../../hoc/Aux'
+import AuthContext from '../../../context/auth-context'
+
 
 import classes from './Person.css';
 
@@ -22,7 +24,9 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
         return (
             <Aux>
-                {this.props.isAuth ? <p>Authenticated</p> : <p>Please log in</p>}
+                <AuthContext.Consumer>
+                    {(context) => context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
+                </AuthContext.Consumer>
                 <p key="i1" onClick={this.props.click}> I 'm {this.props.name} and I
                     am {this.props.age} years old!</p>
                 <p key="i2"> {this.props.children}</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 import withClass from '../hoc/withClass'
 import Aux from '../hoc/Aux'
+import AuthContext from '../context/auth-context'
 
 class App extends Component {
 
@@ -81,7 +82,7 @@ class App extends Component {
     }
 
     loginHandler = () => {
-        this.state.authenticated = true;
+        this.setState({authenticated: true});
     }
 
     render() {
@@ -104,16 +105,20 @@ class App extends Component {
                     this.setState({showCockpit: false})
                 }}>Remove Cockpit
                 </button>
-                {this.state.showCockpit ?
-                    <Cockpit
-                        title={this.props.appTitle}
-                        showPersons={this.state.showPersons}
-                        personsLength={this.state.persons.length}
-                        clicked={this.togglePersonsHandler}
-                        login={this.loginHandler}
-                    /> :
-                    null}
-                {persons}
+                <AuthContext.Provider value={{
+                    authenticated: this.state.authenticated,
+                    login: this.loginHandler}}
+                >
+                    {this.state.showCockpit ?
+                        <Cockpit
+                            title={this.props.appTitle}
+                            showPersons={this.state.showPersons}
+                            personsLength={this.state.persons.length}
+                            clicked={this.togglePersonsHandler}
+                        /> :
+                        null}
+                    {persons}
+                </AuthContext.Provider>
             </Aux>
         );
     }
